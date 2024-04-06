8f9602473bb43cd9d8dfa797a39eaeda0b5e7016
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 00:47:09 +0530

    Add files via upload

---
+++ Print_Short.java
@@ -0,0 +1,17 @@
+class Print_Short
+ {
+  /*The Java short keyword is a 
+    primitive data type. It is used
+    to declare variables. It can also be used with methods.
+  */
+ 
+  public static void main(String[]args)
+   {
+    short num=34;
+    System.out.println(num);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:34
+*/
