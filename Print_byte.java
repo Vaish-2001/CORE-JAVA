f93207c794389fceafba11aaeafd16302f703449
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 00:46:27 +0530

    Add files via upload

---
+++ Print_byte.java
@@ -0,0 +1,18 @@
+class Print_byte
+ {
+  public static void main(String[]args)
+   {
+    /*A byte in Java is 8 bits. 
+    It is a primitive data type, meaning it 
+    comes packaged with Java. Bytes can hold 
+    values from -128 to 127.
+    */
+    byte num=15;
+    System.out.println(num);
+   }
+ }
+
+  /*CT:SUCESS
+    RT:15
+   */
+    
