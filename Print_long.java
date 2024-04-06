942d8dee5049ff3153685f587c6e48c7b2af8ccd
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 01:07:23 +0530

    Add files via upload

---
+++ Print_long.java
@@ -0,0 +1,13 @@
+class Print_long
+ {
+   //long is a data type used to store large numerical values
+   public static void main(String[]args)
+    {
+     long num=5632231;
+     System.out.println(num);
+    }
+ }
+
+/*output-CT:SUCESS
+         RT:5632231
+*/
