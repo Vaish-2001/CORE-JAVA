5741cf3b431bc4f672c0713d48ce4249e91e7650
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 01:08:20 +0530

    Add files via upload

---
+++ print_char.java
@@ -0,0 +1,17 @@
+class print_char
+ {
+  /*a char is a primitive data type
+   that's used to represent a single
+   16-bit Unicode character.
+  */
+
+  public static void main(String[]args)
+   {
+    char num='A';
+    System.out.println(num);
+   }
+ }
+
+ /*output:CT:SUCESS
+          RT:A
+ */
