87a57542d825576b4ce42f57c585234c9f969b5b
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 00:54:13 +0530

    Add files via upload

---
+++ Print_int.java
@@ -0,0 +1,13 @@
+class Print_int
+ {
+  //int' is a primitive data type in Java used to store integer values, 
+  public static void main(String[]args)
+   {
+    int num=54;
+    System.out.println(num);
+   }
+ }
+ 
+ /*output-CT:SUCESS
+          RT:54
+ */
