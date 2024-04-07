5d9cd8393055aa2ccff38584fd9e5a281c542232
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 11:35:17 +0530

    Add files via upload

---
+++ Syntax_Of_If_Else.java
@@ -0,0 +1,22 @@
+class Syntax_Of_If_Else
+{
+	public static void main(String [] soie)
+	{
+		int age=35;
+		
+		if(age>=18)
+		{
+			System.out.println("Eligible for voting");
+		}
+		else
+		{
+			System.out.println("Not Eligible for voting");
+		}
+	}
+}
+
+/*
+OUTPUT
+CT: success:
+RT: success: Eligible for voting
+*/
