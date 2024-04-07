9a122a83ec29a42a9b94ba9494a2b095afd9fa2a
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 10:44:57 +0530

    Add files via upload

---
+++ Syntax_of_If.java
@@ -0,0 +1,13 @@
+class Syntax_of_If
+ {
+   public static void main(String[]args)
+    {
+     int age=20;
+     if(age>=18);
+      {
+        System.out.println("congrats! you are eligible for voting");
+      }
+    }
+ }
+
+//output:congrats! you are eligible for voting
