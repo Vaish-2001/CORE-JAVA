5349e0de5344caed2cc493c4ecfcf207097a61f5
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 07:52:30 +0530

    Add files via upload

---
+++ Print_double.java
@@ -0,0 +1,13 @@
+class Print_double
+ {
+  //used to identify a data type in Java that is used to store decimal numbers,
+
+  public static void main(String[]args)
+   {
+    double nuum=3.6;
+    System.out.println(num);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:3.6
