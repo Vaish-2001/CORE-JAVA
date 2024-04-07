58013d5645f0b120e95c9284191dd1fc1f214677
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 18:47:42 +0530

    Add files via upload

---
+++ Do_while_Example.java
@@ -0,0 +1,28 @@
+class Do_while_Example
+{
+	public static void main(String [] dwe)
+	{
+		int value=1; //declaration
+		
+		do
+		{
+			System.out.println(value);
+			value++; //value=value+1;
+		}while(value<=10);
+	}
+}
+
+/*
+OUTPUT
+CT: success;
+RT: success: 1
+			 2
+			 3
+			 4
+			 5
+			 6
+			 7
+			 8
+			 9
+			 10
+*/
