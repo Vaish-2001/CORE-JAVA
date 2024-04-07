2943c3151175b3e868e4f948c32cdfba83243661
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 09:06:01 +0530

    Add files via upload

---
+++ Compound_Assign_operator.java
@@ -0,0 +1,16 @@
+//this is binary compound assign operator
+
+class Compound_Assign_operator
+ { 
+  public static void main(String[]args)
+   {
+    int num1=3;
+    int num2=4;
+    num1*=num2;
+    System.out.println(num1);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:12
+*/
