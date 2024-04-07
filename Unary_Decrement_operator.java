37522c1e29f9c68a32e564d1d45de04a42d876a6
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 08:14:58 +0530

    Add files via upload

---
+++ Unary_Decrement_operator.java
@@ -0,0 +1,15 @@
+//this is example of unary decrement operator
+
+class Unary_Decrement_operator
+ {
+  public static void main(String[]args)
+   {
+     int a=4;
+     int result=--a -5 -a--;
+     System.out.println(result);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:-5
+*/
