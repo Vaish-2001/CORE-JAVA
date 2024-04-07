1269f160e91e8f5aaf3cf2000e243b34392a744f
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 19:08:27 +0530

    Add files via upload

---
+++ Buzz_If_Else.java
@@ -0,0 +1,22 @@
+class Buzz_If_Else
+{
+	public static void main(String [] bie)
+
+	{
+		int number=50;
+		if(number%7==0 || number%10==7)
+		{
+			System.out.println("Buzz Number");
+		}
+		else
+		{
+			System.out.println("Not Buzz Number");
+		}
+	}
+}
+
+/*
+OUTPUT
+CT: success;
+RT: success: Not Buzz Number
+*/
