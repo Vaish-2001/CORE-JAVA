8d8b6cab60ee33ce30493c15528e9e637b8002d7
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 09:11:12 +0530

    Add files via upload

---
+++ Vovel_Or_Not.java
@@ -0,0 +1,15 @@
+class Vovel_Or_Not
+{
+  public static void main(String[]args)
+    {
+       char cha='Y';
+       String result=(cha=='A'||cha=='E'||cha=='I'||cha=='O'||cha=='U')?("This is vovel"):("This is not vovel");
+       System.out.println(result);
+    }
+ }
+/*
+OUTPUT
+CT: success
+RT: success: Character is  not vovel
+*/
+
