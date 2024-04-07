d39b80991d6f3ee192de2d975fbf4486a0bc89d3
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 09:54:40 +0530

    Add files via upload

---
+++ Greater_Than.java
@@ -0,0 +1,17 @@
+
+class Greater_Than
+{
+	public static void main(String []gt)
+	{
+		int age =23;
+		
+		boolean result = age > 18;
+		
+		System.out.println("Person is eligible for voting "+result);
+	}
+}
+
+/*
+CT: success;
+RT: success: Person is eligible for voting true
+*/
