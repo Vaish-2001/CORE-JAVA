f60745e0acce4a75c60870460ae1f93bb441db14
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 09:16:43 +0530

    Add files via upload

---
+++ Uppercase_lowercase.java
@@ -0,0 +1,14 @@
+//this is the ternary operator
+class Uppercase_lowercase
+{ 
+  public static void main(String[]args)
+   {
+    char n='G';
+    String result=(n>=65&&n<=90)?("the given value is uppercase"):("the given value is lowercase");
+    System.out.println(result);
+   }
+}
+
+/*output:CT:SUCESS
+         RT:the given value is uppercase
+*/
