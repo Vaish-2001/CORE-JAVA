c6a6144a12ff59de3fbe3ce3c4b9726d191a400a
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 16:32:48 +0530

    Add files via upload

---
+++ Short_To_Byte.java
@@ -0,0 +1,14 @@
+class Short_To_Byte
+{
+	public static void main(String[] stb)
+	{
+		short value=-456;
+		byte value1= (byte)value;
+		System.out.println(value1);
+	}
+}
+
+/* Output
+CT: success;
+RT: success: 56
+*/
