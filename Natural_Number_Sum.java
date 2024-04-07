f5ecbfaa4b4470627d5d29a58ac1a6ca20e2b610
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 18:04:57 +0530

    Add files via upload

---
+++ Natural_Number_Sum.java
@@ -0,0 +1,18 @@
+class Natural_Number_Sum
+{
+	public static void main(String [] nns)
+	{
+		//int sum = 1+2+3+4+5+6+7+8+9+10;
+		//System.out.println("Sum of natural number in range from 1 to 10 is: " + sum);
+		
+		//OR
+		
+		int nr=10;
+		System.out.println("Sum of natural number in range from 1 to 10 is: " + (nr*(nr+1)/2));
+	}
+}
+
+/* Output
+CT: success;
+RT: success: Sum of natural number in range from 1 to 10 is: 55
+*/
