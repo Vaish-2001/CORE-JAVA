a214f1d195946a1d84778e22cb064a7f777d0507
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 16:30:40 +0530

    Add files via upload

---
+++ Star_1_Square_Fill_Pattern.java
@@ -0,0 +1,24 @@
+class Star_1_Square_Fill_Pattern
+{
+	public static void main(String [] sfp)
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=5; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+/*
+OUTPUT
+CT: Success;
+RT: Success: * * * * *
+	     * * * * *
+	     * * * * *
+	     * * * * *
+	     * * * * *
+*/
