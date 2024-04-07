bd14854217c75af7a2f812c93acf9bc94e3d8bd6
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 07:56:17 +0530

    Add files via upload

---
+++ Print_String.java
@@ -0,0 +1,12 @@
+class Print_String
+ {
+  // Strings are the type of objects that can store the character of values and in Java, 
+  public static void main(String[]args)
+    {
+     String a="vaish";
+     System.out.println(a);
+    }
+ }
+
+/*output:CT:SUCESS
+         RT:vaish
