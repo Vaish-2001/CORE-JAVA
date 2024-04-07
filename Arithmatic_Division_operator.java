af37a0c1d196344e4a50fedc3168724fa2ecd903
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 08:44:31 +0530

    Add files via upload

---
+++ Arithmatic_Division_operator.java
@@ -0,0 +1,15 @@
+//this is binary arithmatic operator 
+
+class Arithmatic_Division_operator
+ {
+  public static void main(String[]args)
+   {
+    int num1=3;
+    int num2=6;
+    System.out.println(num1/num2);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:0
+*/
