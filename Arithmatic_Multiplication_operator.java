2de7b185e80a7bcf4b2be6c2ac74e8e48b59e656
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 08:42:25 +0530

    Add files via upload

---
+++ Arithmatic_Multiplication_operator.java
@@ -0,0 +1,15 @@
+//this is binary arithmatic operator 
+
+class Arithmatic_Multiplication_operator
+ {
+  public static void main(String[]args)
+   {
+    int num1=23;
+    int num2=26;
+    System.out.println(num1*num2);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:598
+*/
