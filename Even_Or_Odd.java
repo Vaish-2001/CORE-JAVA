dd522c97d75265c84a75bbc72b9d2ca4cf502c43
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 18:34:06 +0530

    Add files via upload

---
+++ Even_Or_Odd.java
@@ -0,0 +1,17 @@
+// Check the given character is even or odd using ternary operator
+
+class Even_Or_Odd
+{
+	public static void main (String [] eoo)
+	{
+		char n='A';
+		String result=(n%2==0)?("Character is even"):("Character is odd");
+		System.out.println(result);
+	}
+}
+
+/*
+OUTPUT:
+CT: success;
+RT: success: Character is odd
+*/
