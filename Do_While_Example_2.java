907f822ccea07fe5e1fe72e2d6a9dc075ee1d9a8
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 18:52:19 +0530

    Add files via upload

---
+++ Do_While_Example_2.java
@@ -0,0 +1,39 @@
+class Do_While_Example_2
+{
+	public static void main(String [] dwe)
+	{
+		int v=2;
+		
+		do
+		{
+			System.out.println(v);
+			v+=2;
+		}while(v<=40);
+
+	}
+}
+
+/*
+OUTPUT
+CT: success;
+RT: success: 2
+			 4
+			 6
+			 8
+			 10
+			 12
+			 14
+			 16
+			 18
+			 20
+			 22
+			 24
+			 26
+			 28
+			 30
+			 32
+			 34
+			 36
+			 38
+			 40
+*/
