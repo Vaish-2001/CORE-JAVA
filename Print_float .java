fb66ed5f306e0f2be7c78a5401d51dbb9d5b5a94
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 07:39:28 +0530

    Add files via upload

---
+++ Print_float .java
@@ -0,0 +1,14 @@
+class Print_float 
+ {
+  //identifies a data type that can hold decimal numbers, 
+
+  public static void main(String[]args)
+   {
+    float num=3.4f;
+    System.out.println(num);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:3.4
+*/
