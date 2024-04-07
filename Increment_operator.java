9a15d4fe20fb26d0afba9780faf20790dcfb2863
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 08:09:26 +0530

    Add files via upload

---
+++ Increment_operator.java
@@ -0,0 +1,14 @@
+// this is the example of unary increment operator
+
+class Increment_operator
+ {
+  public static void main(String[]args)
+   {
+     int a=2;
+     int result = a++ +a + ++a;
+     System.out.println(result);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:9
