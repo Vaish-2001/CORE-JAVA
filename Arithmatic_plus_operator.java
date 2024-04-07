ef2b977db5db7956b4f4b38007c232514909bf9b
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 08:22:06 +0530

    Add files via upload

---
+++ Arithmatic_plus_operator.java
@@ -0,0 +1,15 @@
+//this is binary arithmatic operator 
+
+class Arithmatic_plus_operator
+ {
+  public static void main(String[]args)
+   {
+    int age =23;
+    String name ="vaishnavi";
+    System.out.println(age +"-"+name);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:23-vaishnavi
+*/
