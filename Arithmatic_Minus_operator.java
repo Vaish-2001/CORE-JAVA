4467bf6dc747550351ea5b722acf875765a1b022
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-04-07 08:26:14 +0530

    Add files via upload

---
+++ Arithmatic_Minus_operator.java
@@ -0,0 +1,15 @@
+//this is binary arithmatic operator 
+
+class Arithmatic_Minus_operator
+ {
+  public static void main(String[]args)
+   {
+    int age1=23;
+    int age2=26;
+    System.out.println(age1-age2);
+   }
+ }
+
+/*output:CT:SUCESS
+         RT:-3
+*/
